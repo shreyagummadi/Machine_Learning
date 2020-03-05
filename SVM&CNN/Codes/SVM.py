8f3419c3a85f6aaff896164d4a58d94a9da4540b
shreyagummadi <55266109+shreyagummadi@users.noreply.github.com>
2020-03-05 11:25:31 -0500

    Add files via upload

---
+++ SVM&CNN/Codes/SVM.py
@@ -0,0 +1,53 @@
+
+import time
+
+from sklearn import svm, metrics
+from sklearn.datasets import fetch_openml
+from sklearn.model_selection import train_test_split
+from sklearn import decomposition
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
+
+''' get mnist data'''
+X, y = fetch_openml('mnist_784', version=1, return_X_y=True)
+
+''' normalize the data'''
+X = X/255.0
+
+
+opt = int(input("Select 1.PCA 2.LDA "))
+
+''' choose between LDA and PCA'''
+if opt ==1:
+    pca = decomposition.PCA()
+    pca.n_components = 500
+    data = pca.fit_transform(X)
+
+elif opt ==2:
+    lda = LDA(n_components = 2)
+    data = lda.fit_transform(X,y)
+
+X_train, X_test, y_train, y_test = train_test_split(data, y, test_size=0.15, shuffle =False)
+
+classifier = svm.SVC(kernel = 'linear')
+
+''' learning on train samples'''
+start_time = time.time()
+print('Start')
+classifier.fit(X_train, y_train)
+end_time = time.time()
+print('End')
+t = end_time-start_time
+print('Time taken to run: ', t)
+
+
+''' evaluate on test samples'''
+predicted = classifier.predict(X_test)
+
+
+print("Classification report for classifier %s:\n%s\n"
+      % (classifier, metrics.classification_report(y_test, predicted)))
+      
+cm = metrics.confusion_matrix(y_test, predicted)
+print("Confusion matrix:\n%s" % cm)
+
+print("Accuracy={}".format(metrics.accuracy_score(y_test, predicted)))
---
+++ SVM&CNN/ReadMe.txt
@@ -0,0 +1,20 @@
+
+
+Modules needed to run the programs:
+Tensorflow (version 1.13.0)
+Keras
+
+
+Run:
+SVM.py
+SVM_Kernel.py
+Keras_CNN.py
+
+
+After running the SVM,SVM_Kernel select the mode of dimensionality reduction: PCA or LDA. 
+
+
+
+
+
+
